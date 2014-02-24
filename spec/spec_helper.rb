869dd7333cf7c3f7376abb49e696b4875bedf4ce
Integralist <mark.mcdx@gmail.com>
2014-02-24 13:09:22 +0000

    Remove commented out lines [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,8 @@
-# $: << File.join(File.dirname(__FILE__),"..", "lib")
-
 require 'pry'
 require 'alephant/publisher'
 require 'alephant/publisher/models/writer'
 require 'alephant/publisher/models/queue'
 require 'alephant/publisher/models/render_mapper'
-# require 'logger'
 require 'alephant/renderer'
 require 'alephant/support/parser'
 
