f65a2589334ebc6812ab6552eeee8e77f96b40e1
Steven Jack <stevenmajack@gmail.com>
2014-07-30 12:04:49 +0100

    Tests for adding aws account id to queue retrieval method

--- spec/publisher_spec.rb
+++ spec/publisher_spec.rb
@@ -1,20 +1,41 @@
 require 'spec_helper'
 
 describe Alephant::Publisher do
-  let(:options)  { Alephant::Publisher::Options.new }
-  let(:instance) { Alephant::Publisher.create(options) }
+  let(:options)       { Alephant::Publisher::Options.new }
+  let(:queue)         { double('AWS::SQS::Queue', :url => nil ) }
+  let(:queue_double)  { double('AWS::SQS::QueueCollection', :[] => queue, :url_for => nil) }
+  let(:client_double) { double('AWS::SQS', :queues => queue_double) }
 
   before(:each) do
-    allow_any_instance_of(Alephant::Publisher::SQSHelper::Queue).to receive(:initialize)
+    expect(AWS::SQS).to receive(:new).and_return(client_double)
   end
 
-  describe "#initialize(opts = {}, logger)" do
+  describe ".create" do
     it "sets parser, sequencer, queue and writer" do
-      queue_double = double('AWS::SQS::QueueCollection', :named => 'test-queue')
-      sqs_double = double('AWS::SQS', :queues => queue_double)
-
-      expect(AWS::SQS).to receive(:new).and_return(sqs_double)
+      instance = Alephant::Publisher.create(options)
       expect(instance.queue).to be_a Alephant::Publisher::SQSHelper::Queue
+    end
+
+    context "with account" do
+      it "creates a queue with an account number in the option hash" do
+        options = Alephant::Publisher::Options.new
+        options.add_queue({ :sqs_queue_name => 'bar', :aws_account_id => 'foo' })
+
+        expect(queue_double).to receive(:url_for).with('bar', { :queue_owner_aws_account_id => 'foo' })
+
+        Alephant::Publisher.create(options)
+      end
+    end
+
+    context "without account" do
+      it "creates a queue with an empty option hash" do
+        options = Alephant::Publisher::Options.new
+        options.add_queue({ :sqs_queue_name => 'bar' })
+
+        expect(queue_double).to receive(:url_for).with('bar', {})
+
+        Alephant::Publisher.create(options)
+      end
     end
   end
 end
