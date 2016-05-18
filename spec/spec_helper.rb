e0bb63baf4adaaeaaea1cb7f71f1b55964f575e9
Darren Oakley <daz.oakley@gmail.com>
2016-05-18 12:21:25 +0100

    Make it obvious this is no longer "a thing"
    - Remove all of the remaining code.
    - Update the README to only say that this is deprecated and what has
      replaced it.

--- spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'pry'
-
-require 'aws-sdk-v1'
-require 'alephant/publisher'
-require 'alephant/publisher/views'
-require 'alephant/publisher/views/json'
-require 'alephant/publisher/views/html'
-
